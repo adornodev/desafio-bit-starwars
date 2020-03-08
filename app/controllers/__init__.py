2290240b97b999b374c30b76373579acdfb2233b
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-08 16:40:16 -0300

    Adicionado rotas para inserir um planeta; Adicionado o controller da entidade Planeta

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 
 from app.controllers.planets_controller import index as planets_index
 from app.controllers.planets_controller import destroy as planets_destroy
+from app.controllers.planets_controller import insert as planets_insert
 
 
-
+# Add routes for Planet entity
 planet_blueprints = Blueprint('planets', 'api', url_prefix='/planets')
 planet_blueprints.add_url_rule('/<int:planet_id>', view_func=planets_index, methods=['GET'])
 planet_blueprints.add_url_rule('/', view_func=planets_destroy, methods=['DELETE'])
+planet_blueprints.add_url_rule('/', view_func=planets_insert, methods=['POST'])
--- app/controllers/planets_controller.py
+++ app/controllers/planets_controller.py
@@ -1,11 +1,24 @@
 from flask import request, jsonify
 from app.models.planet import Planet
 from app import mongo
+from app.helpers.utils import send_response, bson_serialize
 
 def index(planet_id):
   # Realizar pesquisa paginada no banco
-  return jsonify({'success': True})
+  return send_response({'success': True})
+
+def insert():
+  payload = request.get_json(force=True)
+
+  # Initialize a Planet object
+  planet  = Planet(payload['name'], payload['climate'], payload['terrain'])
+
+  # Insert Planet on mongo
+  res = mongo.db.planets.insert_one(bson_serialize(planet))
+
+  return send_response(planet)
+
 
 def destroy():
   # Realizar leitura do req.body
-  return jsonify({'success': True})
+  return send_response({'success': True})
