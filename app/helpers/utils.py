323b48475d1ef2624095e1559fcbab8857db3a69
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-08 20:19:16 -0300

    Correções nas funções da classe utils

--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -1,34 +1,11 @@
-from flask import make_response, jsonify
-from datetime import datetime
-from flask_pymongo import ObjectId
+import json
 
-def json_serialize(data):
-    result = {}
-    
-    if isinstance(data, dict):
-        result = data
-    else:
-        input_data = data.__dict__
-        for prop in input_data:
-            if isinstance(input_data[prop], (datetime, ObjectId)):
-                result[prop] = str(input_data[prop])
-            else:
-                result[prop] = input_data[prop]
+from flask import make_response
+from app.models.response import Response
+from app.helpers.json_encoder import CustomJSONEncoder
 
-    return result
+def send_response(result:Response):
 
-def bson_serialize(data):
-    result = {}
-    
-    if isinstance(data, dict):
-        result = data
-    else:
-        input_data = data.__dict__
-        result = input_data
-
-    return result
-
-def send_response(result):
-    resp = make_response(jsonify(json_serialize(result)))
+    resp = make_response(json.dumps(result.to_dict(), cls=CustomJSONEncoder))
     resp.mimetype = 'application/json'
     return resp
