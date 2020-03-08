23bf995d363204fe4d96e474de8bef03479d1f2b
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-08 20:19:46 -0300

    Criado método to_dict na classe Planeta

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,4 +1,4 @@
-from app import mongo
+import json
 from .base_model import BaseModel
 
 class Planet(BaseModel):
@@ -8,9 +8,26 @@ class Planet(BaseModel):
         self.name = name
         self.climate = climate
         self.terrain = terrain
+    
+    def to_dict(self):
+        result = {}
+        
+        result['name'] = self.name
+        result['climate'] = self.climate
+        result['terrain'] = self.terrain
+        result['created_at'] = self.created_at #str(self.created_at)
+        result['updated_at'] = self.updated_at #str(self.updated_at)
+        result['_id'] = self._id #str(self._id)
 
-    @classmethod
-    def get(self):
-        return []
+        return result
+    
+    @staticmethod
+    def remove_invisible_fields(ignore_id = True):
+        fields = {'created_at': 0, 'updated_at': 0}
+
+        if ignore_id:
+            fields['_id'] = 0
+
+        return fields
 
 planet_model=Planet()
