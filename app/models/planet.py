2e0c035b43b93df9d8e65bfbe8666a8fbb118de0
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-09 20:25:15 -0300

    Adicionado campo "swapi_id" e "films" na classe Planet

--- app/models/planet.py
+++ app/models/planet.py
@@ -3,16 +3,21 @@ from .base_model import BaseModel
 
 class Planet(BaseModel):
 
-    def __init__(self, name = '', climate = '', terrain = ''):
+    def __init__(self, name='', climate= '', terrain= '', swapi_id= -1, films= -1):
         super().__init__()
-        self.name = name
-        self.climate = climate
-        self.terrain = terrain
-    
+
+        self.name = None if not name else name.strip()
+        self.climate = [] if not climate else [climate] if ',' not in climate else [c.strip() for c in climate.split(',')]
+        self.terrain = [] if not terrain else [terrain] if ',' not in terrain else [t.strip() for t in terrain.split(',')]
+        self.swapi_id = swapi_id
+        self.films = films
+
     def to_dict(self):
         result = {}
         
         result['name'] = self.name
+        result['swapi_id'] = self.swapi_id
+        result['films'] = self.films
         result['climate'] = self.climate
         result['terrain'] = self.terrain
         result['created_at'] = self.created_at
