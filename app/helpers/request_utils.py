c5eac48a237b1a681f083c996c6b7b7cbd0d3edc
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-09 20:24:54 -0300

    Refatoração no nome das classes utilitárias

--- app/helpers/request_utils.py
+++ app/helpers/request_utils.py
@@ -1,11 +1,11 @@
 import json
+import requests
 
 from flask import make_response
 from app.models.response import Response
-from app.helpers.json_encoder import CustomJSONEncoder
+from app.helpers.json_encoder_utils import CustomJSONEncoder
 
 def send_response(result:Response):
-
     resp = make_response(json.dumps(result.to_dict(), cls=CustomJSONEncoder))
     resp.mimetype = 'application/json'
     return resp
